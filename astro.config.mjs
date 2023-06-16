f5d5cc627413b2dfe2aaadac68ca32aed3eb5fca
cxx <0001001bill@gmail.com>
2023-06-16 11:16:23 -0500

    update to starlight
    the official astro doc site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,54 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import react from '@astrojs/react';
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-  // Enable Preact to support Preact JSX components.
-  preact(),
-  // Enable React for the Algolia search component.
-  react(), tailwind()],
-  site: `https://knowledge.muprosoftware.com`
+    starlight({
+      title: "Knowledge",
+      description: "Knowledge for MuPRO",
+      social: {
+        github: "https://github.com/muprosoftware/knowledge.muprosoftware.com",
+      },
+      editLink: {
+        baseUrl: 'https://github.com/muprosoftware/knowledge.muprosoftware.com/edit/main/',
+      },
+      sidebar: [
+        {
+          label: "Prepare",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Introduction", link: "/en/introduction/" },
+            { label: "For User", link: "/en/prepare/user/" },
+            { label: "For Developer", link: "/en/prepare/developer/" },
+          ],
+        },
+        {
+          label: "Run simulation",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Linux Basics", link: "/en/simulation/linux/" },
+            { label: "Start Simulation", link: "/en/simulation/run/" },
+            { label: "Q & A", link: "/en/simulation/questions/" },
+          ],
+        },
+        {
+          label: "Programming",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Git", link: "en/programming/git" },
+            { label: "CMake", link: "en/programming/cmake" },
+            { label: "Versioning", link: "en/programming/versioning" },
+            { label: "CICD", link: "en/programming/cicd" },
+            { label: "Documentation", link: "en/programming/docs" },
+            { label: "Collaboration", link: "en/programming/contribute" },
+            { label: "Best practices", link: "en/programming/recommendation" },
+          ],
+        },
+      ],
+    }),
+  ],
+
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: { service: { entrypoint: "astro/assets/services/sharp" } },
 });
